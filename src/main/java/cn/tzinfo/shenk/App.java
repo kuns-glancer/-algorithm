5db1d1b902f330e8ded3ec2b41f8672bf35e8f79
shenk <kuns_ML@126.com>
2020-05-08 21:31:21 +0800

    first commit

---
+++ src/main/java/cn/tzinfo/shenk/App.java
@@ -0,0 +1,13 @@
+package cn.tzinfo.shenk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
